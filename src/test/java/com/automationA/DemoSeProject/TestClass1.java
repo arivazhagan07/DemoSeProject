8ff202986c8b4a66243bc6a707b75f0f7ca8436a
Arivazhagan <arivazhagan.r07@gmail.com>
2020-11-16 15:16:07 +0530

    my git commit

---
+++ src/test/java/com/automationA/DemoSeProject/TestClass1.java
@@ -0,0 +1,57 @@
+package com.automationA.DemoSeProject;
+
+import java.util.concurrent.TimeUnit;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;import org.testng.annotations.BeforeMethod;
+
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+
+
+public class TestClass1 {
+	
+	WebDriver driver = null;
+
+	@BeforeMethod
+	public void launchBrowser( ) {
+
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+	driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+	driver.manage().window().fullscreen();
+
+	}
+
+	@Test
+	public void Test1( ) {
+
+	driver.navigate().to("https://www.google.com/?safe=active&ssui=on");
+	System.out.println("Test1 Title is : "+driver.getTitle());
+	}
+
+	@Test
+	public void Test2( ) {
+	driver.navigate().to("https://www.google.com/?safe=active&ssui=on");
+	System.out.println("Test2 Title is : "+driver.getTitle());
+
+	}
+
+
+	@Test
+	public void Test3( ) {
+	driver.navigate().to("https://www.google.com/?safe=active&ssui=on");
+	System.out.println("Test3 Title is : "+driver.getTitle());
+
+	}
+
+	@AfterMethod
+	public void afterMethod( ) {
+
+	driver.quit();
+
+	}
+
+}
